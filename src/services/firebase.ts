285292942fff3a73a5cfca1a92921709c3fdd306
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-17 17:55:15 -0300

    chore: configurando a conexão com o firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGIN_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const database = firebase.firestore();
+
+export { database };
+export default firebase;
