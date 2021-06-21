c00aed629c4e6fe181dda50291455dda4b52cc66
Daniel Silva <dani3l.dxp@gmail.com>
2021-06-21 16:34:31 -0300

    feat: colocando o title da page dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import useAuth from "../hooks/useAuth";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
@@ -5,10 +7,15 @@ export default function Dashboard() {
   const { customer, signOut } = useAuth();
 
   return (
-    <main>
-      <h1>Dashboard: {customer?.email}</h1>
-      <button onClick={signOut}>Sair</button>
-    </main>
+    <>
+      <Head>
+        <title>Dashboard | littlepaws.</title>
+      </Head>
+      <main>
+        <h1>Dashboard: {customer?.email}</h1>
+        <button onClick={signOut}>Sair</button>
+      </main>
+    </>
   );
 }
 
